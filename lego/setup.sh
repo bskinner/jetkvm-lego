4cd04faa85c21768e4d80c509a921d7ec14b5e2b
blake <blake@rootpath.dev>
2026-07-15 10:22:32 -0400

    add fetching of the lego binary

--- lego/setup.sh
+++ lego/setup.sh
@@ -9,6 +9,27 @@ wget \
     --no-check-certificate \
     "https://curl.se/ca/cacert.pem"
 
+export SSL_CERT_FILE="${LEGO_HOME}/certs/ca-certificates.crt"
+
+LEGO_VERSION="5.2.2"
+LEGO_RELEASE_URL="https://github.com/go-acme/lego/releases/download/v${LEGO_VERSION}/lego_v${LEGO_VERSION}_linux_armv7.tar.gz"
+LEGO_RELEASE_FILE="lego_v${LEGO_VERSION}_linux_armv7.tar.gz"
+LEGO_RELEASE_DEST_PATH="${LEGO_HOME}/lego_${LEGO_VERSION}"
+echo "Fetching Lego v${LEGO_VERSION} binary..."
+
+(
+    mkdir "$LEGO_RELEASE_DEST_PATH"
+    cd "$LEGO_RELEASE_DEST_PATH"
+    wget \
+        -O "$LEGO_RELEASE_FILE" \
+        --no-check-certificate \
+        "$LEGO_RELEASE_URL"
+    tar -zxf "$LEGO_RELEASE_FILE"
+)
+
+mkdir "${LEGO_HOME}/bin"
+cp "${LEGO_RELEASE_DEST_PATH}/lego" "${LEGO_HOME}/bin/lego"
+rm -rf "$LEGO_RELEASE_DEST_PATH"
 
 if [ ! -h "/etc/init.d/S22cron" ]; then
     ln -s "${LEGO_HOME}/init.d/S22cron" "/etc/init.d/S22cron"
